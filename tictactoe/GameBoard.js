ebd5ccb22db28aabf94a45ca63481cc0a2ca4a76
luiscachi <lufer_cc@hotmail.com>
2015-07-21 16:55:26 -0400

    class game bord

--- tictactoe/GameBoard.js
+++ tictactoe/GameBoard.js
@@ -1,1 +1,22 @@
-//GameBoard.js
+/**
+ * Created by .
+ */
+var Board = function(){
+    this.grid = [];
+};
+
+Board.prototype.initGrid = function(){
+    var clearGrid = 1;
+    for(var i = 0 ; i<3 ; i++)
+    {
+        this.grid[i] = new Array(3);
+    }
+    for(var i = 0 ; i<3 ; i++)
+    {
+        for(var j = 0 ; j<3 ; j++)
+        {
+            this.grid[i][j] = clearGrid.toString();
+            clearGrid++;
+        }
+    }
+};
