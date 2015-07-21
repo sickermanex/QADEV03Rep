192099bdcb205c161b4268de2c34231f564e70f0
sickermanex <sickermanex@gmail.com>
2015-07-21 16:53:08 -0400

    The main class of the tictactoe project

---
+++ tictactoe/MainGame.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Manu on 18/07/2015.
+ */
+var board = require('./GameBoard.js');
+
+var TTT = function(){
+    this.movements = 5;
+    if(arguments.length == 0 )
+    {
+        if(process.argv[2].length>= this.movements && process.argv[2].length<=9)
+        {
+            this.moves = process.argv[2];
+            this.winner = '';
+            this.board = null;
+            this.initGame();
+            this.gameResults();
+        }
+        else
+        {
+            this.error();
+        }
+    }
+    else
+    {
+        if(arguments[0].toString().length >= this.movements && arguments[0].toString().length<=9)
+        {
+            this.moves = arguments[0].toString();
+            this.winner = '';
+            this.board = null;
+            this.initGame();
+            this.gameResults();
+        }
+        else
+        {
+            this.error();
+        }
+    }
+
+};
